3cf770e5e2c0e73b12865a2f285a589784d57a1f
Gabrielle Soares <55507831+devgabrielle@users.noreply.github.com>
2022-10-16 18:31:26 -0300

    Create styles.js

---
+++ Components/Address/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+padding: 40px 20px;
+`;
+
+export const AddressMenu = styled.TouchableOpacity`
+display: flex;
+flex-direction: row;
+`;
+
+export const Location = styled.Text`
+color: #333;
+font-size: 16px;
+font-weight: bold;
+`;
