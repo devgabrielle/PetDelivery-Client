13c5624c36219797aba1c7a2b7922cbb46457eab
Gabrielle Soares <55507831+devgabrielle@users.noreply.github.com>
2022-10-16 18:32:20 -0300

    Create styles.js

---
+++ Components/Schedule/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.TouchableOpacity`
+display: flex;
+align-items: center;
+
+`;
+
+export const Image = styled.Image`
+margin-top: -10px;
+width: 360px;
+height: 160px;
+border: 5px solid #A16CE6;
+border-radius: 10px;
+`;
