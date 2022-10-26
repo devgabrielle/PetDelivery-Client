db6cccd263503273be380f919dbca7c69b897497
Gabrielle Soares <55507831+devgabrielle@users.noreply.github.com>
2022-10-26 20:36:39 -0300

    Create index.js

---
+++ Pages/PreviousRequests/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Purchesed from '../../Components/Purchesed';
+
+import { Container } from './styles';
+
+export default function PreviousRequests() {
+    return (
+        <Container>
+            <Purchesed />
+        </Container>
+    );
+};
