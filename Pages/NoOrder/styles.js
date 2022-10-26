358e3a4a7067d4fe5dfc5e30f4167d82ff32fb4e
Gabrielle Soares <55507831+devgabrielle@users.noreply.github.com>
2022-10-26 20:35:10 -0300

    Create styles.js

---
+++ Pages/NoOrder/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+flex: 1;
+background-color: #FFF;
+align-items: center;
+justify-content: center;
+`;
+
+export const Image = styled.Image`
+width: 250px;
+height: 250px;
+`;
+
+export const Warning = styled.Text`
+margin-top: 25px;
+font-size: 20px;
+font-weight: 400;
+align-self: center;
+`;
+
+export const Message = styled.View`
+align-items: center;
+`;
+
+export const Suggetion = styled.Text`
+margin-top: 15px;
+font-size: 15px;
+color: #999;
+`;
