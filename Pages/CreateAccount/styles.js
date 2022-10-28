8d8957b8298e984619c6d0d5220d08abdebc2ef4
Gabrielle Soares <55507831+devgabrielle@users.noreply.github.com>
2022-10-28 01:02:17 -0300

    Create styles.js

---
+++ Pages/CreateAccount/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+flex: 1;
+display: flex;
+align-items: center;
+`;
+
+export const Logo = styled.Image`
+width: 250px;
+height: 250px;
+`;
+
+export const Title = styled.Text`
+color: black;
+font-size: 28;
+font-weight: bold;
+`;
+
+export const SuggetionBox = styled.View`
+flex-direction: row;
+`;
+
+export const Btn = styled.TouchableOpacity``;
+
+export const TextButton = styled.Text`
+text-decoration: underline;
+`;
+
+export const SubTitle = styled.Text`
+font-size: 15px;
+`;
+
+export const InputContainer = styled.View`
+align-items: center;
+margin: 20px;
+`;
+
+export const ButtonContainer = styled.View`
+margin-top: 30px;
+`;
