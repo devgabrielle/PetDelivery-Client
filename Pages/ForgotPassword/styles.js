06def9c7e8ab3d5b7300feb55c2c3c837d37e0f7
Gabrielle Soares <55507831+devgabrielle@users.noreply.github.com>
2022-10-28 01:01:35 -0300

    Create styles.js

---
+++ Pages/ForgotPassword/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+display: flex;
+align-items: center;
+
+`;
+
+export const Logo = styled.Image`
+margin-top: 20px;
+width: 300px;
+height: 300px;
+`;
+
+export const Title = styled.Text`
+margin-top: 20px;
+color: black;
+font-size: 30;
+font-weight: bold;
+`;
+
+export const SubTitle = styled.Text`
+margin: 15px 80px ;
+font-size: 15px;
+text-align: center;
+`;
+
+export const InputContainer = styled.View`
+align-items: center;
+margin: 20px;
+`;
+
+export const ButtonContainer = styled.View`
+margin-top: 50px;
+`;
+
